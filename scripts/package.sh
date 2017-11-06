68996fa85e43f7d382e2472a236f5423b3b86c64
Dariusz Filipiak <darekf77@gmail.com>
2017-11-06 18:13:18 +0100

    asdasd

---
+++ scripts/package.sh
@@ -0,0 +1,4 @@
+sh scripts/delete_tsc_files.sh
+node_modules/.bin/tsc
+release-it
+sh scripts/delete_tsc_files.sh
---
+++ src/base-class.ts
@@ -0,0 +1,131 @@
+import * as _ from "lodash";
+
+import { Log, Level } from 'ng2-logger';
+const log = Log.create('datatable base model')
+
+export interface model {
+    propertyPath: string;
+    class: { new (any?): BaseModelWrap<any> };
+}
+
+
+export abstract class BaseModelWrap<T> {
+    constructor(data?: T, insideModelsMapping?: model[]) {
+
+        
+        // prepare model
+        if (Array.isArray(insideModelsMapping) && insideModelsMapping.length > 0 && data != undefined) {
+            for (var i = 0; i < insideModelsMapping.length; i++) {
+                let m = insideModelsMapping[i];
+                // log.i('propertyPath', m.propertyPath)
+                // log.i('class', m.class)
+                // log.i('data', data)
+                let o = _.get(data, m.propertyPath);
+                // log.i('o', o)
+                if (!o) continue;
+                if (Array.isArray(o)) {
+                    // log.i('is arrrat', o)
+                    for (let k = 0; k < (o as any[]).length; k++) {
+                        o[k] = new m.class(o[k]);
+                        // log.i('o[k]', o[k])
+                    }
+                    // log.i('is arrrat afetr', o)
+                } else {
+                    // log.i('is object')
+                    _.set(data, m.propertyPath, new m.class(o))
+                }
+                // log.i('data after', data)
+            }
+        }
+        this.__insideModelsMapping = insideModelsMapping;
+        this.__data = data;
+    }
+    private __insideModelsMapping?: model[]
+    protected __data: T;
+    protected get data(): T {
+        return !this.__data ? <T>{} : this.__data;
+    }
+
+    public static rows<T, M = BaseModelWrap<T>>(data: T[], TCreator:
+        { new (any?): BaseModelWrap<T>; }, filterEmptyRow = false): M[] {
+
+        let res: BaseModelWrap<T>[] = [];
+        if (filterEmptyRow) {
+            let d = new TCreator();
+            res.push((d).__forFilter())
+        }
+        if (data && Array.isArray(data)) data.forEach(f => res.push(new TCreator(f)));
+        return res as any;
+    }
+    private __forFilter(): BaseModelWrap<T> {
+        this._isEmpty = true;
+        return this;
+    }
+
+    public abstract clone(): BaseModelWrap<T>;
+
+    public static clone<T>(models: BaseModelWrap<T>[]) {
+        return models.map(m => m.clone());
+    }
+
+
+    public raw(): T {
+        let data: T = JSON.parse(JSON.stringify(this.__data));
+
+        // prepare raw data
+        if (Array.isArray(this.__insideModelsMapping) && this.__insideModelsMapping.length > 0 && data != undefined) {
+            for (var i = 0; i < this.__insideModelsMapping.length; i++) {
+                let m = this.__insideModelsMapping[i];
+                let o = _.get(data, m.propertyPath);
+                let oi = _.get(this.__data, m.propertyPath);
+                // log.i('propertyPath', m.propertyPath)
+                // log.i('class', m.class)
+                // log.i('data', data)
+                if (!o) continue;
+                if (Array.isArray(o)) {
+                    // log.i('is array')
+                    for (let k = 0; k < (o as any[]).length; k++) {
+                        o[k] = (oi[k] as BaseModelWrap<any>).raw();
+                    }
+                } else {
+                    // log.i('is object')
+                    _.set(data, m.propertyPath, (oi as BaseModelWrap<any>).raw());
+                }
+                // log.i('transformed', data)
+            }
+        }
+        return data;
+    }
+
+
+    private _isEmpty = false;
+    get isEmpty() {
+        return this._isEmpty;
+    }
+    public selected?: boolean = false;
+    public get id() {
+        return this.data['id'];
+    }
+    public set id(v) {
+        this.data['id'] = v;
+    }
+
+    public toString = () => {
+        return `item: id=${this.id}`
+    }
+
+}
+
+
+// class DD extends BaseModelWrap<{}> {
+//     public clone(): DD {
+//         let raw: {} = this.raw();
+//         return new DD(raw);
+//     }
+
+// }
+
+
+// let d = DD.rows<any, DD>([], DD);
+
+
